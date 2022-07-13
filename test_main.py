75d7229549d759b6ffbc584cf460e01a58fa6fe7
josiokoko <josiokoko@gmail.com>
2022-07-12 23:20:46 -0600

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -25,8 +25,6 @@ class TestJSONLoaderMethods(unittest.TestCase):
         self.assertEqual(self.movies[0]['id'], 'tt0111161')
 
 if __name__ == '__main__':
-    # runner = xmlrunner.XMLTestRunner(output='reports')
-    # unittest.main(testRunner=runner)
-    
-    with open('reports/results.xml', 'r') as output:
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output),failfast=False, buffer=False, catchbreak=False)
+    runner = xmlrunner.XMLTestRunner(output='reports')
+    unittest.main(testRunner=runner)
+   
